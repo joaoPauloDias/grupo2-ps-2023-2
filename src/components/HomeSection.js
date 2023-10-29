75f121e2b5a837f13c16a52f258c4de46d009bf4
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2023-10-29 00:02:23 -0300

    adding homesection title and button

--- src/components/HomeSection.js
+++ src/components/HomeSection.js
@@ -17,7 +17,7 @@ const HomeSection = () => {
             <span className={styles.redText}>IDE</span>
             <span>X</span>
           </h2>
-          <button></button>
+          <button className={styles.startButton}>INICIAR</button>
         </div>
       </div>
     </section>
