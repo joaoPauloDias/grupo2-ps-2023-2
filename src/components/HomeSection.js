48cf83626b045e99f0f752c00b131841b414d837
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2023-10-28 23:32:08 -0300

    adding homesection and new font

---
+++ src/components/HomeSection.js
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+import styles from '../styles/HomeSection.module.css';
+
+const HomeSection = () => {
+  return (
+    <section className={styles.homeSection}>
+      <div className={styles.content}>
+        <Image
+          src="/../public/img/neko-anotando.png"
+          alt="Neko anotando"
+          width={400}
+          height={400}
+        />
+        <div className={styles.contentTitle}>
+          <h2>
+            <span>POKE</span>
+            <span className={styles.redText}>IDE</span>
+            <span>X</span>
+          </h2>
+          <button></button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import Header from '../components/Header';
+import HomeSection from '../components/HomeSection';
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <HomeSection />
+    </>
+  );
 }
