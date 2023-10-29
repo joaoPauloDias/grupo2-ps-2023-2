3ebcadf9b85169d0a01704d0f22ab8b0fca28274
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2023-10-28 22:30:24 -0300

    adding header component and global font

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+import styles from '../styles/Header.module.css';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
