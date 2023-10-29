214b1b9c4bbb19981ac0477764080ffd71b74eab
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2023-10-28 22:52:46 -0300

    adding header to index

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,8 @@
 import '@/styles/globals.css';
-import Header from '../components/Header';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Header />
       <Component {...pageProps} />
     </>
   );
